5eb921a834a8e79228496897f1c8915be32e8c10
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-11-17 18:50:51 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,70 @@
+
+/**
+ * @param {number[]} candies
+ * @param {number} numberOfConsecutiveCandiesToGive
+ * @return {number}
+ */
+var shareCandies = function (candies, numberOfConsecutiveCandiesToGive) {
+    const frequencyCandies = createFrequencyCandies(candies);
+    let currentNumberOfUniqueFlavorsRetainedAfterGiving = findTotalNumberOfUniqueCandies(frequencyCandies);
+    if (numberOfConsecutiveCandiesToGive === 0) {
+        return currentNumberOfUniqueFlavorsRetainedAfterGiving;
+    }
+
+    for (let i = 0; i < numberOfConsecutiveCandiesToGive; ++i) {
+        --frequencyCandies[candies[i]];
+        if (frequencyCandies[candies[i]] === 0) {
+            --currentNumberOfUniqueFlavorsRetainedAfterGiving;
+        }
+    }
+    let maxNumberOfUniqueFlavorsRetainedAfterGiving = currentNumberOfUniqueFlavorsRetainedAfterGiving;
+
+    for (let i = numberOfConsecutiveCandiesToGive; i < candies.length; ++i) {
+        --frequencyCandies[candies[i]];
+        if (frequencyCandies[candies[i]] === 0) {
+            --currentNumberOfUniqueFlavorsRetainedAfterGiving;
+        }
+
+        ++frequencyCandies[candies[i - numberOfConsecutiveCandiesToGive]];
+        if (frequencyCandies[candies[i - numberOfConsecutiveCandiesToGive]] === 1) {
+            ++currentNumberOfUniqueFlavorsRetainedAfterGiving;
+        }
+
+        maxNumberOfUniqueFlavorsRetainedAfterGiving = Math.max(
+                maxNumberOfUniqueFlavorsRetainedAfterGiving,
+                currentNumberOfUniqueFlavorsRetainedAfterGiving);
+    }
+
+    return maxNumberOfUniqueFlavorsRetainedAfterGiving;
+};
+
+/**
+ * @param {number[]} candies
+ * @return {number[]}
+ */
+function createFrequencyCandies(candies) {
+    let maxCandyValue = 0;
+    for (let candy of candies) {
+        maxCandyValue = Math.max(maxCandyValue, candy);
+    }
+
+    const frequencyCandies = new Array(maxCandyValue + 1).fill(0);
+    for (let candy of candies) {
+        ++frequencyCandies[candy];
+    }
+    return frequencyCandies;
+}
+
+/**
+ * @param {number[]} frequencyCandies
+ * @return {number}
+ */
+function findTotalNumberOfUniqueCandies(frequencyCandies) {
+    let totalNumberOfUniqueCandies = 0;
+    for (let frequency of frequencyCandies) {
+        if (frequency > 0) {
+            ++totalNumberOfUniqueCandies;
+        }
+    }
+    return totalNumberOfUniqueCandies;
+}
